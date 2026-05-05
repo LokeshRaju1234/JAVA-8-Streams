9f6d88e88c085452c4725612e254e430d6381f23
LokeshRaju1234 <loki65628@gmail.com>
2026-05-05 22:55:52 +0530

    Done with all various different streams

---
+++ src/DifferentStreamCreation.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.util.stream.Stream;
+
+public class DifferentStreamCreation
+{
+    public static void main(String[] args)
+    {
+        ///From collection
+        List<Integer> salaryList = Arrays.asList(1,2,3,4,5,6);
+        Stream<Integer> streamFromIntegerList = salaryList.stream();
+
+        //from Array
+        Integer [] salaryArray = {1,2,3,4,5,6};
+        Stream<Integer> streamFromArray = Arrays.stream(salaryArray);
+
+        //from static method
+        Stream<Integer> streamFromStaticMethod = Stream.of(1,2,3,4,5);
+
+        //from stream Builder
+        Stream.Builder<Integer> streambuilder = Stream.builder();
+        streambuilder.add(1).add(2).add(3);
+
+        //from stream iterate
+        //iterate -->will keep on iterating
+        //we need to give the starting value
+        Stream<Integer> streamFromIterate =
+                Stream.iterate(1000,(Integer n) -> n + 5000).limit(5);
+
+
+    }
+}
