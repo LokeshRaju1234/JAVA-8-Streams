d34395dccbc1d947b3f872ab24aaf48103603d5f
LokeshRaju1234 <loki65628@gmail.com>
2026-05-06 23:20:54 +0530

    implemented dictinct  operation

---
+++ src/IntermediateOperations/distinct.java
@@ -0,0 +1,21 @@
+package IntermediateOperations;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class distinct {
+    public static void main(String [] args) {
+
+        Integer[] arr = {1, 5, 2, 7, 4, 4, 2, 0, 9};
+
+        //dictinct is used to remove the duplicate values and keep only unique values
+        Stream<Integer> result = Arrays.stream(arr).distinct();
+
+        List<Integer> res = result.collect(Collectors.toList());
+        System.out.println(res);
+    }
+
+}
