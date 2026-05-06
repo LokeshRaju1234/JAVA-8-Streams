74b3ce9d1b605cd73a560fe0ea2456bf76980fab
LokeshRaju1234 <loki65628@gmail.com>
2026-05-06 23:06:51 +0530

    implemented flatmap operation

---
+++ src/IntermediateOperations/flatmap.java
@@ -0,0 +1,30 @@
+package IntermediateOperations;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class flatmap {
+    public static void main(String[] args)
+    {
+        //used to iterate over each element of the complex collection and helps to flatten
+        //into single line
+        List<List<String>> sentenceList = Arrays.asList(Arrays.asList("I","LOVE","YOU"),
+                Arrays.asList("Your","Concepts","ARE","Clear"),
+                Arrays.asList("ITS","VERY","EASY"));
+
+        Stream<String>  wordstream = sentenceList.stream()
+                .flatMap((List<String> sentence) -> sentence.stream());
+        List<String> res = wordstream.collect(Collectors.toList());
+        System.out.println(res);
+
+        //you can also use map with flatmap in order to transfrom the value
+        Stream<String>  wordstream1 = sentenceList.stream()
+                .flatMap((List<String> sentence) -> sentence.stream()
+                        .map((String name) -> name.toLowerCase()));
+
+        List<String> res1 = wordstream1.collect(Collectors.toList());
+        System.out.println(res1);
+    }
+}
