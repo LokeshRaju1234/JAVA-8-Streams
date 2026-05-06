4934e8c16a08fc8d131150e83ff0986014148415
LokeshRaju1234 <loki65628@gmail.com>
2026-05-06 22:54:09 +0530

    implemented map operation

--- src/IntermediateOperations/Filter.java
+++ src/IntermediateOperations/Filter.java
@@ -1,4 +1,17 @@
 package IntermediateOperations;
 
-public class Filter {
+import javax.print.DocFlavor;
+import java.util.*;
+import java.util.stream.*;
+
+public class Filter
+{
+    public static void main(String[] args)
+    {
+        Stream<String> nameStream = Stream.of("Hello","EVERY","BODY");
+        Stream<String> filteredStream = nameStream.filter((String name) ->
+                name.length() <= 4);
+        List<String> result = filteredStream.collect(Collectors.toList());
+        System.out.println(result);
+    }
 }
--- src/IntermediateOperations/Map.java
+++ src/IntermediateOperations/Map.java
@@ -1,4 +1,18 @@
 package IntermediateOperations;
 
-public class Map {
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Map
+{
+    public static void main(String[] args)
+    {
+        //map is used to transfrom each element
+        Stream<String> nameStream = Stream.of("Hello","EVERY","BODY");
+        Stream<String> transformedResult = nameStream.map((String name) ->
+                name.toLowerCase());
+        List<String> result = transformedResult.collect(Collectors.toList());
+        System.out.println(result);
+    }
 }
